7e977f7fcc282ee046bcc4b233b76d3a9c6d796e
acepitcher2009 <69870798+acepitcher2009@users.noreply.github.com>
2020-12-18 11:01:11 -0600

    Movie app

--- src/components/Main.js
+++ src/components/Main.js
@@ -13,7 +13,7 @@ const Main = () => {
     const [searchValue, setSearchValue] = useState('');
 
     const getMovieRequest = async (searchValue) => {
-        const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=3e118d2e`;
+        const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=3e118d2e`;
 
         const response = await fetch(url);
         const responseJson = await response.json();
